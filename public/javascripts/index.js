e5aef7ddd771d786858790e61f220c703b387b97
Brayan Rafael Gallardo Sepulveda <brayan_gallardo@yahoo.com>
2019-04-28 20:42:25 -0700

    Created table with hoverable tooltip for owner_id

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,107 +2,110 @@ $(document).ready(function() {
   $.ajax({
     url: "/projects?page=1",
     type: "GET",
-
     success: function(data) {
       createTable(data);
     }
   });
 });
 
+var tooltipData = {};
+
+var clientId = "moG119pnGbK2BxCzliJ4EzFezKm41Ev6OMvebyUerHnm7nrP";
+var clientSecret = "Xu6XNriu77w4FofDcLNZlTAY2m3u37M8FgYitvZoEQXQElOF";
+var userKey = "557T5zaOC4yzJDfs";
+var apiKey = "?api_key=" + userKey;
 function createTable(data) {
-  <table
-    id="dtBasicExample"
-    class="table table-striped table-bordered table-sm"
-    cellspacing="0"
-    width="100%"
-  >
-    <thead>
-      <tr>
-        <th class="th-sm">Name</th>
-        <th class="th-sm">Position</th>
-        <th class="th-sm">Office</th>
-        <th class="th-sm">Age</th>
-        <th class="th-sm">Start date</th>
-        <th class="th-sm">Salary</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Jackson Bradshaw</td>
-        <td>Director</td>
-        <td>New York</td>
-        <td>65</td>
-        <td>2008/09/26</td>
-        <td>$645,750</td>
-      </tr>
-      <tr>
-        <td>Olivia Liang</td>
-        <td>Support Engineer</td>
-        <td>Singapore</td>
-        <td>64</td>
-        <td>2011/02/03</td>
-        <td>$234,500</td>
-      </tr>
+  var html = "";
+  for (var row = 0; row < data.per_page; row++) {
+    html +=
+      "<tr><td>" +
+      data.projects[row].name +
+      "</td><td><span class='tool-tip'>" +
+      data.projects[row].owner_id +
+      "</span><div id='" +
+      data.projects[row].owner_id +
+      "'" +
+      "class='tool-tip-details'></div></td><td>" +
+      data.projects[row].summary +
+      "</td></tr>";
+  }
 
-      <tr>
-        <td>Serge Baldwin</td>
-        <td>Data Coordinator</td>
-        <td>Singapore</td>
-        <td>64</td>
-        <td>2012/04/09</td>
-        <td>$138,575</td>
-      </tr>
-      <tr>
-        <td>Zenaida Frank</td>
-        <td>Software Engineer</td>
-        <td>New York</td>
-        <td>63</td>
-        <td>2010/01/04</td>
-        <td>$125,250</td>
-      </tr>
-      <tr>
-        <td>Zorita Serrano</td>
-        <td>Software Engineer</td>
-        <td>San Francisco</td>
-        <td>56</td>
-        <td>2012/06/01</td>
-        <td>$115,000</td>
-      </tr>
-      <tr>
-        <td>Jennifer Acosta</td>
-        <td>Junior Javascript Developer</td>
-        <td>Edinburgh</td>
-        <td>43</td>
-        <td>2013/02/01</td>
-        <td>$75,650</td>
-      </tr>
+  $(".table_body").append(html);
 
-      <tr>
-        <td>Michael Bruce</td>
-        <td>Javascript Developer</td>
-        <td>Singapore</td>
-        <td>29</td>
-        <td>2011/06/27</td>
-        <td>$183,000</td>
-      </tr>
-      <tr>
-        <td>Donna Snider</td>
-        <td>Customer Support</td>
-        <td>New York</td>
-        <td>27</td>
-        <td>2011/01/25</td>
-        <td>$112,000</td>
-      </tr>
-    </tbody>
-    <tfoot>
-      <tr>
-        <th>Name</th>
-        <th>Position</th>
-        <th>Office</th>
-        <th>Age</th>
-        <th>Start date</th>
-        <th>Salary</th>
-      </tr>
-    </tfoot>
-  </table>;
+  function appendHTML(owner_id) {
+    var html_append = "<div>";
+    html_append +=
+      "<div class='owner-text'><b>Username</b> " +
+      tooltipData[owner_id].username +
+      "</div>";
+    html_append +=
+      "<div class='owner-text'><b>Screen Name</b> " +
+      tooltipData[owner_id].screen_name +
+      "</div>";
+    html_append +=
+      "<div class='owner-text'><b>About me</b> " +
+      tooltipData[owner_id].about_me +
+      "</div>";
+    html_append +=
+      "<div class='owner-text'><b>Location</b> <i class='fa fa-map-marker'></i> " +
+      tooltipData[owner_id].location +
+      "</div>";
+    html_append +=
+      "<div class='owner-text'><b># of Projects</b> <i class='fa fa-code-fork'></i> " +
+      tooltipData[owner_id].projects +
+      "</div>";
+    html_append +=
+      "<div class='owner-text'><b>Skulls</b> " +
+      tooltipData[owner_id].skulls +
+      "</div>";
+    html_append +=
+      "<div class='owner-text'><b>Followers</b> <i class='fa fa-users'></i> " +
+      tooltipData[owner_id].followers +
+      "</div>";
+    html_append += "</div>";
+
+    $("#" + owner_id).append(html_append);
+  }
+
+  $(".tool-tip").mouseenter(function() {
+    var owner_id = $(this).html();
+    $("#" + owner_id).css({
+      display: "inline",
+      position: "absolute",
+      color: "#111",
+      border: "1px solid #dca",
+      background: "#fffaf0"
+    });
+
+    var owner_url = "http://api.hackaday.io/v1/users/" + owner_id + apiKey;
+    if (!(owner_id in tooltipData)) {
+      $.ajax({
+        url: owner_url,
+        type: "GET",
+
+        success: function(data) {
+          tooltipData[owner_id] = {
+            username: data.username,
+            screen_name: data.screen_name,
+            about_me: data.about_me,
+            location: data.location,
+            projects: data.projects,
+            skulls: data.skulls,
+            followers: data.followers
+          };
+          appendHTML(owner_id);
+        }
+      });
+    } else {
+      appendHTML(owner_id);
+    }
+  });
+
+  $(".tool-tip").mouseleave(function() {
+    var owner_id = $(this).html();
+    $("#" + owner_id).html("");
+    $("#" + owner_id).css({
+      display: "none"
+    });
+  });
 }
